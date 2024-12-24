0492e522f8d8f2380bc8dbd3cb504052b3c60a30
tommyngx <tommyfederation@gmail.com>
2024-12-24 16:49:41 +1100

    test shap png save

--- DeepsurvUTS/evaluate.py
+++ DeepsurvUTS/evaluate.py
@@ -953,7 +953,7 @@ def plot_shap_values_for_ml_model(model, X_train, y_train, X_val, scaler, cols_x
 
     # Plot SHAP waterfall plot for the first validation sample
     print("Generating SHAP waterfall plot for the first validation sample...")
-    shap.waterfall_plot(shap_values_val[0])
+    shap_water = shap.waterfall_plot(shap_values_val[0])
     if save_folder:
         save_path = f"{save_folder}/results/shap_waterfall.png"
         plt.savefig(save_path, format='png')
