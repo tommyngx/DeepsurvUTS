fbca30d1664b117d5c49d69d84b1683b360de9b4
tommyngx <tommyfederation@gmail.com>
2025-01-09 06:34:11 +1100

    fx

--- run/kaplan_plot.py
+++ run/kaplan_plot.py
@@ -122,7 +122,7 @@ def plot_kaplan_meier_with_models(y_time, y_censored, models, X_test, models_to_
 
     # Save the plot if save_folder is provided
     if save_folder:
-        save_path = f"{save_folder}/results/kaplan_meier.png"
+        save_path = f"{save_folder}/kaplan_meier_{title.replace(' ', '_')}.png"
         plt.savefig(save_path, format='png')
 
     # Show the plot if show_plot is True
