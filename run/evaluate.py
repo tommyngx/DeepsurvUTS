028d04cc74208618e25629a698dc69323637f70a
tommyngx <tommyfederation@gmail.com>
2025-01-08 15:50:09 +1100

    fix

--- run/evaluate.py
+++ run/evaluate.py
@@ -374,7 +374,7 @@ def plot_brier_curves_with_color_list(brier_curves, model_name_map=None, save_fo
 
     # Define the color list
     color_list = [
-        "#2ca02c","#ff7f0e",  "#9467bd", "#d62728", "#8c564b",
+        "#2ca02c", "#8c564b", "#9467bd", "#d62728", "#ff7f0e",
         "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"
     ]
     #color_list = plt.get_cmap('Dark2')
