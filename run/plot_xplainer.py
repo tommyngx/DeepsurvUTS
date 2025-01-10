932d515901a14831d73310c8ef79f23d413dc3df
tommyngx <tommyfederation@gmail.com>
2025-01-10 17:07:52 +1100

    save new

--- run/plot_xplainer.py
+++ run/plot_xplainer.py
@@ -20,43 +20,43 @@ def plot_shap_values_from_explainer(shap_values_val, X_val, save_folder, model_n
     """
     
     # Plot SHAP global bar plot
-    print("Generating SHAP global bar plot...")
+    print(f"Generating SHAP global bar plot... for {model_name}")
     shap.plots.bar(shap_values_val, max_display=10, show=False)
     plt.gcf().set_size_inches(11,6)
     if save_folder:
         save_path = f"{save_folder}/shap_global_bar_{model_name}.png"
         plt.savefig(save_path, format='png',bbox_inches='tight', dpi=150)
         plt.close()
-        print(f"SHAP global bar plot saved at: {save_path}")
+        #print(f"SHAP global bar plot saved at: {save_path}")
 
     # Plot SHAP local bar plot for the first validation sample
-    print("Generating SHAP local bar plot for the first validation sample...")
+    print(f"Generating SHAP local bar plot for the first validation sample... for {model_name}")
     shap.plots.bar(shap_values_val[0],  show=False)
     plt.gcf().set_size_inches(11,6)
     if save_folder:
         save_path = f"{save_folder}/shap_local_bar_{model_name}.png"
         plt.savefig(save_path, format='png',bbox_inches='tight', dpi=150)
         plt.close()
-        print(f"SHAP local bar plot saved at: {save_path}")
+        #print(f"SHAP local bar plot saved at: {save_path}")
 
     # Plot SHAP waterfall plot for the first validation sample
-    print("Generating SHAP waterfall plot for the first validation sample...")
+    print(f"Generating SHAP waterfall plot for the first validation sample... for {model_name}")
     shap.plots.waterfall(shap_values_val[0])
     if save_folder:
         shap.plots.waterfall(shap_values_val[0], show=False)
         save_path = f"{save_folder}/shap_waterfall_{model_name}.png"
         plt.savefig(save_path, format='png', bbox_inches='tight', dpi=150)
         plt.close()
-        print(f"SHAP waterfall plot saved at: {save_path}")
+        #print(f"SHAP waterfall plot saved at: {save_path}")
 
     # Plot SHAP summary plot for validation dataset
-    print("Generating SHAP summary plot for validation dataset...")
+    print(f"Generating SHAP summary plot for validation dataset... for {model_name}")
     shap.summary_plot(shap_values_val, X_val, show=False)
     if save_folder:
         save_path = f"{save_folder}/shap_summary_{model_name}.png"
         plt.savefig(save_path, format='png', bbox_inches='tight', dpi=150)
         plt.close()
-        print(f"SHAP summary plot saved at: {save_path}")
+        #print(f"SHAP summary plot saved at: {save_path}")
 
     # Plot SHAP dependence plot for the most important feature
     #top_feature = X_val.columns[np.argmax(shap_values_val.values.mean(axis=0))]
