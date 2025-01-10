8d8f9060e9044f15d1f9d30bfc5d34a5aa4830a6
tommyngx <tommyfederation@gmail.com>
2025-01-10 13:07:00 +1100

    fxo

--- run/plot_xplainer.py
+++ run/plot_xplainer.py
@@ -22,7 +22,7 @@ def plot_shap_values_from_explainer(shap_values_val, X_val, save_folder, model_n
     
     # Plot SHAP global bar plot
     print("Generating SHAP global bar plot...")
-    plt.figure(figsize=(14, 8))
+    plt.gcf().set_size_inches(14,8)
     shap.plots.bar(shap_values_val, max_display=10, show=False)
     if save_folder:
         save_path = f"{save_folder}/shap_global_bar_{model_name}.png"
@@ -32,7 +32,7 @@ def plot_shap_values_from_explainer(shap_values_val, X_val, save_folder, model_n
 
     # Plot SHAP local bar plot for the first validation sample
     print("Generating SHAP local bar plot for the first validation sample...")
-    plt.figure(figsize=(14, 8))
+    plt.gcf().set_size_inches(14,8)
     shap.plots.bar(shap_values_val[0], show=False)
     if save_folder:
         save_path = f"{save_folder}/shap_local_bar_{model_name}.png"
